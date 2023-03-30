256abe630ab5708a92730d3b4c5b04b8a9844c76
piratejas <47509562+piratejas@users.noreply.github.com>
2023-03-30 13:16:43 +0100

    file setup; practised creating containers and sprites

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,38 @@
-import { Application, Sprite } from 'pixi.js'
+import { Application, Sprite, Container } from "pixi.js";
 
 const app = new Application({
-	view: document.getElementById("pixi-canvas") as HTMLCanvasElement,
-	resolution: window.devicePixelRatio || 1,
-	autoDensity: true,
-	backgroundColor: 0x6495ed,
-	width: 640,
-	height: 480
+  view: document.getElementById("pixi-canvas") as HTMLCanvasElement,
+  resolution: window.devicePixelRatio || 1,
+  autoDensity: true,
+  backgroundColor: 0x6495ed,
+  width: 640,
+  height: 480,
 });
 
+const conty: Container = new Container();
+conty.x = 200;
+conty.y = 0;
+app.stage.addChild(conty);
+
+const secondConty: Container = new Container();
+secondConty.x = 300;
+secondConty.y = 0;
+app.stage.addChild(secondConty);
+
+const thirdConty: Container = new Container();
+thirdConty.x = 400;
+thirdConty.y = 0;
+app.stage.addChild(thirdConty);
+
 const clampy: Sprite = Sprite.from("clampy.png");
+clampy.x = 0;
+clampy.y = 0;
 
-clampy.anchor.set(0.5);
+const clampy2: Sprite = Sprite.from("clampy.png");
+clampy2.x = -100;
+clampy2.y = 50;
 
-clampy.x = app.screen.width / 2;
-clampy.y = app.screen.height / 2;
+secondConty.addChild(clampy);
+clampy.addChild(clampy2);
 
-app.stage.addChild(clampy);
+console.log(conty);
